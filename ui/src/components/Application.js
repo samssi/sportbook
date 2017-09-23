27a985eae850403ced1c7ed6de781679a90dffa4
Samssi <samir.hamadi@gmail.com>
2017-09-23 14:37:06 +0300

    Fix application location

---
+++ ui/src/components/Application.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Footer from "./Footer"
+
+const Application = () => (
+    <div>
+        <div>SportBook</div>
+        <Footer />
+    </div>
+);
+
+export default Application;
--- ui/src/containers/Application.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Application = () => (
-    <div>SportBook</div>
-);
-
-export default Application;
