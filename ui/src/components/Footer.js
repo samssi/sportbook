77c43171259c6785887e866ad9d14129173d11e0
Samssi <samir.hamadi@gmail.com>
2017-09-23 14:37:27 +0300

    Add footer

---
+++ ui/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const footer = {
+    textAlign: "center"
+};
+
+const Footer = () => (
+    <div style={footer}>
+        <p>
+            Powered by <a href="https://github.com/samssi/sportbook">SportBook engine</a>
+        </p>
+    </div>
+)
+
+export default Footer
