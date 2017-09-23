d23c2d571a4baf6336e14400f2b6193b89cb8f54
Samssi <samir.hamadi@gmail.com>
2017-09-23 14:44:30 +0300

    Takes fonts into use

--- ui/src/components/Application.js
+++ ui/src/components/Application.js
@@ -1,8 +1,13 @@
 import React from "react";
 import Footer from "./Footer"
 
+const fontStyle = {
+    fontFamily: "'Indie Flower', cursive",
+    fontSize: "30px"
+}
+
 const Application = () => (
-    <div>
+    <div style={fontStyle}>
         <div>SportBook</div>
         <Footer />
     </div>
