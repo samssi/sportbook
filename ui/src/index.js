f3aa6c0fd3147d639dd6dea6eb17be7f8386bb6c
Samssi <samir.hamadi@gmail.com>
2017-09-23 15:57:19 +0300

    Include reset.css to unify different browser styles

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
-import Application from "./components/Application";
+import Application from "./components/Application/Application";
 import { createStore } from "redux";
 import reducer from "./reducers";
+import "./css/reset.css";
 
 let store = createStore(reducer);
 
