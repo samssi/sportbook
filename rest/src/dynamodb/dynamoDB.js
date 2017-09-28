3db69338cf9e9da482fddecc792151747276fc9d
samssi <samir.hamadi@gmail.com>
2017-09-28 15:57:54 +0300

    Refactor

--- rest/src/dynamodb/dynamoDB.js
+++ rest/src/dynamodb/dynamoDB.js
@@ -1,4 +1,8 @@
+const logger = require("bunyan").createLogger({name: "sportbook-rest-dynamodb-dynamodb"});
 const AWS = require("aws-sdk");
+
+const internalError = "InternalError";
+const databaseConnectionError = "DatabaseConnectionError";
 
 const connect = new AWS.DynamoDB(
     {
@@ -8,4 +12,34 @@ const connect = new AWS.DynamoDB(
     }
 );
 
-module.exports = { connect }
+const returnData = (err, data, res) => {
+    if (err.name === "ResourceNotFoundException" || "NetworkingError") {
+        logger.error(formatErrorMessage(databaseConnectionError, err));
+        res
+            .status(503)
+            .send(errorMessage(databaseConnectionError));
+    } else if (err) {
+        logger.error(formatErrorMessage(internalError, err));
+        res
+            .status(500)
+            .send(errorMessage(internalError));
+    } else {
+        res
+            .status(200)
+            .send(data);
+    }
+}
+
+const formatErrorMessage = (topic, err) => {
+    return topic + ": " + err.name + ":" + err.stack;
+}
+
+const errorMessage = (message) => {
+    return {
+        err: {
+            name: message
+        }
+    }
+}
+
+module.exports = { connect, returnData, internalError, databaseConnectionError }
--- rest/src/handlers/privateHandler.js
+++ rest/src/handlers/privateHandler.js
@@ -3,11 +3,9 @@ const router = require("express").Router();
 const uuidv4 = require("uuid/v4");
 const dateFormat = require("dateformat");
 const runlogQueries = require("../queries/runlogQueries");
-const connect = require("../dynamodb/dynamoDB").connect
+const connect = require("../dynamodb/dynamoDB").connect;
+const returnData = require("../dynamodb/dynamoDB").returnData;
 const kmInMeters = require("../util/calculator").kmInMeters;
-
-const internalError = "InternalError";
-const databaseConnectionError = "DatabaseConnectionError";
 
 router.get("/runlog", (req, res) => {
     connect.query(runlogQueries.getRunlogByUsername("samssi"), (err, data) => returnData(err, data, res));
@@ -19,35 +17,5 @@ router.put("/runlog", (req, res) => {
     console.log(meters);
     connect.putItem(runlogQueries.putRunlogItemForUsername("samssi", meters), (err, data) => returnData(err, data, res));
 });
-
-const returnData = (err, data, res) => {
-    if (err.name === "ResourceNotFoundException" || "NetworkingError") {
-        logger.error(formatErrorMessage(databaseConnectionError, err));
-        res
-            .status(503)
-            .send(errorMessage(databaseConnectionError));
-    } else if (err) {
-        logger.error(formatErrorMessage(internalError, err));
-        res
-            .status(500)
-            .send(errorMessage(internalError));
-    } else {
-        res
-            .status(200)
-            .send(data);
-    }
-}
-
-const formatErrorMessage = (topic, err) => {
-    return topic + ": " + err.name + ":" + err.stack;
-}
-
-const errorMessage = (message) => {
-    return {
-        err: {
-            name: message
-        }
-    }
-}
 
 module.exports = router;
