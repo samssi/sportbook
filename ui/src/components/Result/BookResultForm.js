7c640b46ef60c744570a28879a3410d3642716ab
samssi <samir.hamadi@gmail.com>
2017-09-28 17:47:01 +0300

    Initial ui side validation

--- ui/src/components/Result/BookResultForm.js
+++ ui/src/components/Result/BookResultForm.js
@@ -2,9 +2,29 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 import { nbsp } from "../../helpers/unicodeMarkup"
 
+const required = value => (value ? undefined : 'Required');
+const alphaNumeric = value => (value && /[^0-9 ]/i.test(value) ? 'Only alphanumeric characters' : undefined);
+
 const booking = {
     marginLeft: "50px"
 };
+
+const renderNumeric = ({input, label, type, meta: { touched, error }}) => {
+    return (
+    <div>
+        <label>
+            {label}
+        </label>
+        <input {...input} placeholder={label} type={type} />
+        {touched && 
+        (error &&
+            <span>
+                {error}
+            </span>
+        )}
+    </div>
+    );
+}
 
 let BookResultForm = (props) => {
     const { handleSubmit } = props;
@@ -24,9 +44,11 @@ let BookResultForm = (props) => {
                 <label htmlFor="runInKm">ran for (km)</label>
                 <Field 
                   name="runInKm" 
-                  component="input"
+                  component={renderNumeric}
                   type="text"
                   placeholder="6"
+                  validate={[required, alphaNumeric]}
+                  error={[required, alphaNumeric]}
                   />
                   {nbsp}{nbsp}{nbsp}
                 <a href="#" onClick={handleSubmit}>Submit</a>
